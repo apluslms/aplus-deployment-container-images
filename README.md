36974d3bb605617bc2217e970c46ed4ff7e5767e
Murhu Markus <markus.murhu@aalto.fi>
2023-10-30 10:57:05 +0200

    initial commit after moving repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aplus Helm docker images
+Dockerfiles and associated files required to build docker images used in Aalto's A+ Helm chart deployment
---
+++ gitmanager/docker-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+IMG_NAME=apluslms/gitmanager
+DIR=$(dirname $0)
+TAG=$(cat $DIR/tag)
+ITERATION=00
+for i in $(seq -w 0 99); do
+  ITERATION=$i
+  if ! docker manifest inspect $IMG_NAME:$TAG-$ITERATION; then
+    break
+  fi
+done
+IMG_NAME_FULL=$IMG_NAME:$TAG-$ITERATION
+echo "Building and pushing $IMG_NAME_FULL"
+
+docker build --no-cache --build-arg TAG=$TAG -t $IMG_NAME_FULL $DIR && docker push $IMG_NAME_FULL
